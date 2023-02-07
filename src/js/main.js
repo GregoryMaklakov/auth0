0e5d73790dcbbd8389b9970f759dbf2b49a9c07c
Gregory <maklakov.grigory@gmail.com>
2023-02-07 17:23:01 +0100

    fin section caim, create double-slider not adapted

--- src/js/main.js
+++ src/js/main.js
@@ -2,10 +2,16 @@ import './text-hero-animation';
 import './menu';
 
 import AOS from 'aos';
-import 'aos/dist/aos.css'; 
+import 'aos/dist/aos.css';
+
+// import Swiper JS
+
 
 "use strict";
 
 AOS.init();
+
+
+
 
 
