2f9a3be43f7da388a4a4a9a9e4a23b4565ca3f91
Gregory <maklakov.grigory@gmail.com>
2023-04-06 16:10:49 +0200

    add header scroll animation

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,9 +1,30 @@
-export * from './main';
+export * from "./main";
 
-// Bugrer & menu
-let menuBtn = document.querySelector('.icon-menu');
-let menu = document.querySelector('.menu');
-menuBtn.addEventListener('click', function () {
-    menuBtn.classList.toggle('active');
-    menu.classList.toggle('active');
+const menuBtn = document.querySelector(".icon-menu");
+const menu = document.querySelector(".menu");
+const headerTop = document.querySelector(".header__top");
+const body = document.querySelector("body");
+
+menuBtn.addEventListener("click", function () {
+  menuBtn.classList.toggle("active");
+  menu.classList.toggle("active");
+
+  // проверяем, есть ли у меню класс "active"
+  if (menu.classList.contains("active")) {
+    headerTop.style.display = "none"; // скрываем headerTop
+    body.classList.add("no-scroll");
+  } else {
+    headerTop.style.display = "flex"; // показываем headerTop
+    body.classList.remove("no-scroll");
+  }
+});
+
+window.addEventListener("scroll", () => {
+  // текущая позиция вертикальной прокрутки
+  const scrollPosition = window.scrollY;
+  if (scrollPosition >= 100 && !menu.classList.contains("active")) {
+    headerTop.classList.add("header__top-scroll");
+  } else {
+    headerTop.classList.remove("header__top-scroll");
+  }
 });
